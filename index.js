ed2d9e8747eff24b46580bb6fc945270b724a5c6
Silv <forenks1005@gmail.com>
2022-03-19 10:49:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+window.sr =ScrollReveal();
+
+sr.reveal('.cloud1', {
+   duration: 3000,
+   origin:'left',
+   distance: '-100px',
+   easing: 'cubic-bezier(0.1, 0, 0, 1)',
+});
+
+
+
+sr.reveal('.cloud2',{
+   duration: 3000,
+   origin:'left',
+   distance:'-100px',
+   delay:'100',
+   easing: 'cubic-bezier(0.1, 0, 0, 1)',
+
+});
+
+sr.reveal('.line',{
+   duration: 3000,
+   origin:'top',
+   distance:'-100px',
+   delay:'100',
+   easing: 'cubic-bezier(0.1, 0, 0, 1)',
+
+});
+
+sr.reveal('#line2',{
+   duration: 3000,
+   origin:'top',
+   distance:'-100px',
+   delay:'300',
+   easing: 'cubic-bezier(0.1, 0, 0, 1)',
+
+});
+
+ 
+sr.reveal('.cloud3',{
+   duration: 3000,
+   origin:'left',
+   distance:'-100px',
+   delay:'200',
+   easing: 'cubic-bezier(0.1, 0, 0, 1)',
+   
+});
+
+sr.reveal('.flex-2, .flex-3',{
+   duration: 3000,
+   origin: 'bottom',
+   distance: '100px'
+})
+
+
+
+sr.reveal('.brush-sec',{
+   duration: 2000,
+   origin: 'left',
+   distance: '100px'
+});
+
+sr.reveal('.code-sec',{
+   duration: 2000,
+   origin: 'bottom',
+   distance: '100px'
+});
+
+sr.reveal('.gear_sec',{
+   duration: 2000,
+   origin: 'right',
+   distance: '100px'
+});
+
+sr.reveal('.tag1, .tag2',{
+   duration: 2000,
+   origin: 'bottom',
+   distance: '100px'
+});
+
+
+
+sr.reveal('.buttoms',{
+   duration: 1800,
+   scale: 1.3,
+   easing: 'cubic-bezier(0.1, 0, 0, 1)',
+})
+
+sr.reveal('.tags-btn, .tags-btn2',{
+   duration: 1800,
+   scale: 1.3,
+   easing: 'cubic-bezier(0.1, 0, 0, 1)',
+})
